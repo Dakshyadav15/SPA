55e6f2b2331eee80b2b6ff36fd756c2a071727a9
Dakshyadav15 <dakshyadav15102005@gmail.com>
2026-01-24 23:26:39 +0530

    Update Vite configurations and enhance index.html styling; add vercel.json for routing

--- form_spa/vite.config.js
+++ form_spa/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/form_spa',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: './',
       },
     }),
   ],
